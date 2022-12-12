ae625109d2932907673b6f14e858ec45a2cc3097
Zetro <s.zetro007@gmail.com>
2022-12-12 16:13:53 +0100

    Added GET and POST routes

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const app = express();
 //set up views engine to use ejs template package
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {});
+app.get("/", function (req, res) {
+  return 1;
+});
+
+app.post("/book-ride", function (req, res) {
+  return 1;
+});
