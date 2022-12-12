5fc1f979610be858d18a86c59b3055ab1e0d8a9d
Zetro <s.zetro007@gmail.com>
2022-12-12 16:11:59 +0100

    Created the formModel

---
+++ models/formModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const formSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full Name is required"],
+    },
+    Department: {
+      type: String,
+      required: [true, "Department field is required"],
+    },
+    Level: {
+      type: Number,
+      required: [true, "Level is required"],
+    },
+    phoneNumber: {
+      type: Number,
+      required: [true, "Phone Number is required"],
+    },
+    departureDate: {
+      type: Date,
+      required: [true, "Departure date is required"],
+    },
+    dropOffPoint: {
+      type: String,
+      required: [true, "Drop off point is required"],
+    },
+    noOfLuggage: {
+      type: Number,
+      required: [true, "Number of Luggages is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Form", formSchema);
